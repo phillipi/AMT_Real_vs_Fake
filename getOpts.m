e6e103b89a92b38a2b290a2ae5a5ba3da1a354bd
Phillip Isola <phillip.isola@gmail.com>
2016-11-08 21:55:38 -0800

    bug fixes

--- getOpts.m
+++ getOpts.m
@@ -5,8 +5,9 @@ function [opt] = getOpts(expt_name)
 			opt = getDefaultOpts();
 			
 			opt.which_algs_paths = {'L1cGAN'};%{'L1','GAN','cGAN','L1GAN','L1cGAN'};
+			opt.Nimgs = 100;
 			opt.ut_id = 'asdf';
-			opt.base_url = 'https://people.eecs.berkeley.edu/~phillipi/pix2pix/turk/loss_variations/';
+			opt.base_url = 'https://people.eecs.berkeley.edu/~isola/pix2pix/turk/loss_variations/';
 			opt.instructions_file = './instructions_basic.html';
 			opt.short_instructions_file = './short_instructions_basic.html';
 			opt.consent_file = './consent_basic.html';
