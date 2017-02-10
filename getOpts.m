6d3853c0270835c0aae94166441e78b638122b57
Phillip Isola <phillip.isola@gmail.com>
2017-02-09 22:53:15 -0800

    clarified instructions; updated html instruction/consent templates

--- getOpts.m
+++ getOpts.m
@@ -1,68 +1,19 @@
 function [opt] = getOpts(expt_name)
 	
 	switch expt_name
-		case 'cityscapes_all_variations'
-			opt = getDefaultOpts();
-			
-			opt.which_algs_paths = {'L1cGAN'}; %{'L1','GAN','cGAN','L1GAN','L1cGAN','0layers','1layers','6layers','encoderdecoderL1','encoderdecoderL1cGAN'};
-			opt.Nimgs = 100;
-			opt.ut_id = 'asdf';
-			opt.base_url = 'https://people.eecs.berkeley.edu/~isola/pix2pix/turk/cityscapes/all_variations/';
-			opt.instructions_file = './instructions_basic.html';
-			opt.short_instructions_file = './short_instructions_basic.html';
-			opt.consent_file = './consent_basic.html';
-			opt.use_vigilance = false;
-			opt.im_height = 256;
-			opt.im_width = 256;
-			opt.paired = false;
-			
-		case 'map2sat_all_variations'
+        
+		case 'example_expt'
 			opt = getDefaultOpts();
 		
-			opt.which_algs_paths = {'L1','L1cGAN'};
-			opt.Nimgs = 100;
-			opt.ut_id = '5c484f2f9647ff5b7d25eb2aa1bd60c7';
-			opt.base_url = 'https://people.eecs.berkeley.edu/~isola/pix2pix/turk/map2sat/all_variations/';
-			opt.instructions_file = './instructions_basic.html';
-			opt.short_instructions_file = './short_instructions_basic.html';
-			opt.consent_file = './consent_basic.html';
-			opt.use_vigilance = false;
-			opt.im_height = 256;
-			opt.im_width = 256;
-			opt.paired = false;
-			opt.Nhits_per_alg = 50;
-			
-		case 'sat2map_all_variations'
-			opt = getDefaultOpts();
-		
-			opt.which_algs_paths = {'L1','L1cGAN'};
-			opt.Nimgs = 100;
-			opt.ut_id = '5c484f2f9647ff5b7d25eb2aa1bd60c7';
-			opt.base_url = 'https://people.eecs.berkeley.edu/~isola/pix2pix/turk/sat2map/all_variations/';
-			opt.instructions_file = './instructions_sat2map.html';
-			opt.short_instructions_file = './short_instructions_sat2map.html';
-			opt.consent_file = './consent_basic.html';
-			opt.use_vigilance = false;
-			opt.im_height = 256;
-			opt.im_width = 256;
-			opt.paired = false;
-			opt.Nhits_per_alg = 50;
-		
-		case 'colorization_all_variations'
-			opt = getDefaultOpts();
-		
-			opt.which_algs_paths = {'L1cGAN','L2_zhang2016','classrebal_zhang2016'};
+			opt.which_algs_paths = {'my_alg','baseline_alg'};
 			opt.Nimgs = 1000;
-			opt.ut_id = '41d4680920ca6434cf5b9f412e857f26';
-			opt.base_url = 'https://people.eecs.berkeley.edu/~isola/pix2pix/turk/colorization/all_variations/';
-			opt.instructions_file = './instructions_colorization.html';
-			opt.short_instructions_file = './short_instructions_colorization.html';
+			opt.ut_id = 'unset'; % set this using http://uniqueturker.myleott.com/
+			opt.base_url = 'https://www.mywebsite.com/example_expt_data/';
+			opt.instructions_file = './instructions_basic.html';
+			opt.short_instructions_file = './short_instructions_basic.html';
 			opt.consent_file = './consent_basic.html';
 			opt.use_vigilance = false;
-			opt.im_height = 256;
-			opt.im_width = 256;
 			opt.paired = true;
-			opt.Nhits_per_alg = 50;
 		
 		otherwise
 			error(sprintf('no opts defined for experiment %s',expt_name));
